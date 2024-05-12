3fae0ae3353442d9863a290e285a94b514a20de5
Matt Michie <mmichie@gmail.com>
2024-05-12 16:23:35 -0700

    feat: handle Ctrl-D for graceful shell exit
    Enhance the gosh shell to support graceful termination when Ctrl-D (EOF) is pressed. This update
    aligns the shell's behavior with conventional Unix-like shells, where Ctrl-D is commonly used to
    signal an exit.
    
    Changes:
    - Added error handling within the main input loop to detect and respond to EOF errors explicitly.
    - Implemented a specific check for `bufio.ErrBufferFull` to manage cases where the input buffer
      overflows, providing a clear message and allowing the user to continue.
    - Upon detecting EOF, the shell now prints a farewell message and exits cleanly using `os.Exit(0)`.

--- cmd/main.go
+++ cmd/main.go
@@ -11,16 +11,26 @@ import (
 
 func main() {
 	session := gosh.NewSession() // Initializes a new session
-
-	// Example use: print session start time or user information
 	fmt.Printf("Session started at %v by user %d (%s)\n", session.StartTime, session.UserID, session.UserName)
 
 	reader := bufio.NewReader(os.Stdin)
-
 	fmt.Println("Welcome to gosh Shell")
+
 	for {
 		fmt.Print("> ")
-		input, _ := reader.ReadString('\n')
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			if err == bufio.ErrBufferFull {
+				fmt.Println("Input buffer overflow. Please try again.")
+				continue
+			} else if err.Error() == "EOF" { // Handle EOF when Ctrl-D is pressed.
+				fmt.Println("\nExit command received. Exiting gosh shell.")
+				os.Exit(0)
+			}
+			fmt.Println("An error occurred:", err)
+			continue
+		}
+
 		input = strings.TrimSpace(input)
 		if input == "" {
 			continue
