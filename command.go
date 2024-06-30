16dfac72aeadc6a99346cc64302934ca250605a8
Matt Michie <mmichie@gmail.com>
2024-06-30 10:25:25 -0700

    refactor: simplify command execution and structure
    - Remove Redirection and Background fields from Command
    - Add Stderr field to Command for error handling
    - Simplify simpleExec() to handle built-in and external commands
    - Remove setupRedirection() function
    - Placeholder implementation for pipelineExec()

--- command.go
+++ command.go
@@ -1,6 +1,7 @@
 package gosh
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -12,23 +13,16 @@ import (
 
 type Command struct {
 	*parser.Command
-	Redirections []*Redirection `parser:"@@*"`
-	Background   bool           `parser:"@'&'"`
-	Stdin        io.Reader
-	Stdout       io.Writer
-	StartTime    time.Time
-	EndTime      time.Time
-	Duration     time.Duration
-	TTY          string
-	EUID         int
-	CWD          string
-	ReturnCode   int
-}
-
-type Redirection struct {
-	Operator       string `parser:"@Operator"`
-	Word           string `parser:"@Word"`
-	FileDescriptor int    `parser:"@Int"`
+	Stdin      io.Reader
+	Stdout     io.Writer
+	Stderr     io.Writer
+	StartTime  time.Time
+	EndTime    time.Time
+	Duration   time.Duration
+	TTY        string
+	EUID       int
+	CWD        string
+	ReturnCode int
 }
 
 func NewCommand(input string) (*Command, error) {
@@ -36,7 +30,12 @@ func NewCommand(input string) (*Command, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &Command{Command: parsedCmd}, nil
+	return &Command{
+		Command: parsedCmd,
+		Stdin:   os.Stdin,
+		Stdout:  os.Stdout,
+		Stderr:  os.Stderr,
+	}, nil
 }
 
 func (cmd *Command) Run() {
@@ -56,6 +55,9 @@ func (cmd *Command) Run() {
 	}
 	// Add code to handle other command types (ForLoop, IfCondition, CaseStatement) if needed
 
+	cmd.EndTime = time.Now()
+	cmd.Duration = cmd.EndTime.Sub(cmd.StartTime)
+
 	// Log the command execution details
 	historyManager, err := NewHistoryManager("")
 	if err != nil {
@@ -68,111 +70,42 @@ func (cmd *Command) Run() {
 	}
 }
 
-// Update simple execution to handle command structures
 func (cmd *Command) simpleExec() {
-	if cmd.SimpleCommand == nil {
+	if cmd.SimpleCommand == nil || len(cmd.SimpleCommand.Items) == 0 {
 		return
 	}
-	executable, err := exec.LookPath(cmd.SimpleCommand.Items[0].Value)
-	if err != nil {
-		log.Printf("Command not found: %s", cmd.SimpleCommand.Items[0].Value)
+
+	cmdName := cmd.SimpleCommand.Items[0].Value
+
+	// Check for built-in commands
+	if builtinCmd, ok := builtins[cmdName]; ok {
+		builtinCmd(cmd)
 		return
 	}
-	args := make([]string, len(cmd.SimpleCommand.Items)-1)
+
+	// External command execution
+	cmdArgs := make([]string, len(cmd.SimpleCommand.Items)-1)
 	for i, item := range cmd.SimpleCommand.Items[1:] {
-		args[i] = item.Value
+		cmdArgs[i] = item.Value
 	}
-	process := exec.Command(executable, args...)
-	setupRedirection(process, cmd)
 
-	if cmd.Background {
-		err = process.Start()
-		if err != nil {
-			log.Printf("Error executing %s in background: %v", executable, err)
-			return
-		}
-		log.Printf("Started %s [PID: %d] in background", executable, process.Process.Pid)
+	execCmd := exec.Command(cmdName, cmdArgs...)
+	execCmd.Stdin = cmd.Stdin
+	execCmd.Stdout = cmd.Stdout
+	execCmd.Stderr = cmd.Stderr
+
+	err := execCmd.Run()
+	if err != nil {
+		fmt.Fprintf(cmd.Stderr, "%s: command not found\n", cmdName)
+		cmd.ReturnCode = 1
 	} else {
-		err = process.Run()
-		if err != nil {
-			log.Printf("Error executing %s: %v", executable, err)
-		}
-	}
-	cmd.EndTime = time.Now()
-	cmd.Duration = cmd.EndTime.Sub(cmd.StartTime)
-}
-
-func setupRedirection(process *exec.Cmd, cmd *Command) {
-	for _, redirection := range cmd.Redirections {
-		var file *os.File
-		var err error
-		if redirection.Operator == ">" || redirection.Operator == ">>" {
-			file, err = os.OpenFile(redirection.Word, os.O_CREATE|os.O_WRONLY|(func() int {
-				if redirection.Operator == ">>" {
-					return os.O_APPEND
-				}
-				return os.O_TRUNC
-			}()), 0644)
-		} else if redirection.Operator == "<" {
-			file, err = os.Open(redirection.Word)
-		}
-		if err != nil {
-			log.Printf("Error opening file %s for redirection: %v", redirection.Word, err)
-			return
-		}
-		if redirection.FileDescriptor != 0 {
-			process.ExtraFiles = append(process.ExtraFiles, file)
-		} else {
-			switch redirection.Operator {
-			case ">":
-				process.Stdout = file
-			case "<":
-				process.Stdin = file
-			case ">>":
-				process.Stdout = file
-			}
-		}
+		cmd.ReturnCode = execCmd.ProcessState.ExitCode()
 	}
 }
 
 func (cmd *Command) pipelineExec() {
-	var commands []*exec.Cmd
-	for _, simpleCommand := range cmd.PipelineCommand.Commands {
-		executable, err := exec.LookPath(simpleCommand.Items[0].Value)
-		if err != nil {
-			log.Printf("Command not found: %s", simpleCommand.Items[0].Value)
-			return
-		}
-
-		args := make([]string, len(simpleCommand.Items)-1)
-		for i, item := range simpleCommand.Items[1:] {
-			args[i] = item.Value
-		}
-		command := exec.Command(executable, args...)
-		commands = append(commands, command)
-	}
-
-	for i := 0; i < len(commands)-1; i++ {
-		pipe, err := commands[i].StdoutPipe()
-		if err != nil {
-			log.Printf("Error creating pipe: %v", err)
-			return
-		}
-		commands[i+1].Stdin = pipe
-	}
-
-	for _, command := range commands {
-		err := command.Start()
-		if err != nil {
-			log.Printf("Error starting command: %v", err)
-			return
-		}
-	}
-
-	for _, command := range commands {
-		err := command.Wait()
-		if err != nil {
-			log.Printf("Error waiting for command: %v", err)
-		}
-	}
+	// Implementation for pipeline execution
+	// This is more complex and involves creating pipes between commands
+	// For now, we'll leave this as a placeholder
+	fmt.Fprintln(cmd.Stderr, "Pipeline execution not implemented yet")
 }
