5e21dadbd190eafa05a7bbf2e9d9a7c9ab41e89d
Matt Michie <mmichie@gmail.com>
2024-07-13 07:58:38 -0700

    feat(repl): enhance REPL with readline functionality
    - Add support for command history and line editing
    - Implement Ctrl-C for interrupting evaluation
    - Add Ctrl-D support for exiting the REPL
    - Improve error handling and user feedback

--- m28/repl.go
+++ m28/repl.go
@@ -1,28 +1,48 @@
 package m28
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+	"io"
 	"strings"
+
+	"github.com/chzyer/readline"
 )
 
 // REPL starts a Read-Eval-Print Loop for the M28 Lisp interpreter
 func (i *Interpreter) REPL() {
-	reader := bufio.NewReader(os.Stdin)
+	rl, err := readline.New("m28> ")
+	if err != nil {
+		panic(err)
+	}
+	defer rl.Close()
+
 	fmt.Println("M28 Lisp REPL")
-	fmt.Println("Type 'exit' or 'quit' to exit the REPL")
+	fmt.Println("Type 'exit', 'quit', or use Ctrl-D to exit the REPL")
+	fmt.Println("Use Ctrl-C to interrupt the current evaluation")
+
 	for {
-		fmt.Print("m28> ")
-		input, err := reader.ReadString('\n')
+		input, err := rl.Readline()
 		if err != nil {
+			if err == readline.ErrInterrupt {
+				// Ctrl-C was pressed, just continue to the next prompt
+				continue
+			} else if err == io.EOF {
+				// Ctrl-D was pressed
+				fmt.Println("\nExiting M28 Lisp REPL")
+				break
+			}
 			fmt.Println("Error reading input:", err)
 			continue
 		}
 
 		input = strings.TrimSpace(input)
 		if input == "exit" || input == "quit" {
+			fmt.Println("Exiting M28 Lisp REPL")
 			break
+		}
+
+		if input == "" {
+			continue
 		}
 
 		result, err := i.Execute(input)
@@ -31,8 +51,9 @@ func (i *Interpreter) REPL() {
 		} else {
 			fmt.Println("=>", result)
 		}
+
+		rl.SaveHistory(input)
 	}
-	fmt.Println("Exiting M28 Lisp REPL")
 }
 
 // RunREPL creates a new interpreter and starts the REPL
